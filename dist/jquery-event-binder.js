968ff64e8fd1c82e59eb7acbcc9419439d458d57
Anton Temchenko <zdobrom@gmail.com>
2016-07-14 23:08:54 +0200

    upgrading functionality of library

--- dist/jquery-event-binder.js
+++ dist/jquery-event-binder.js
@@ -1,18 +1,33 @@
 (function( $ ){
     $.fn.extend({
-        eventList: [],
         storeEvents: function(){
             var _this = this;
-            $.each($._data(_this.get(0), 'events'), function () {
-                $j.each(this, function () {
-                    _this.eventList.push({type: this.type, handler: this.handler});
+            if(!_this.eventList){_this.eventList = []}
+            var elemEvents = $._data(_this.get(0), 'events');
+            try {
+                $.each(elemEvents, function () {
+                    $.each(this, function () {
+                        _this.eventList.push({type: this.type, handler: this.handler});
+                    });
                 });
-            });
+            }catch(e){
+                console.error("Element doesn't have any event binders to store");
+            }
             return _this;
         },
-        bindStoredEvents: function(){
+        bindStoredEvents: function(elem){
+            if(!this.eventList){
+                return this;
+            }
+            if(elem){
+                if(!(elem instanceof jQuery)){
+                    elem = $(elem);
+                }
+            }else{
+                elem = this;
+            }
             this.eventList.forEach(function (evElem, i) {
-                _this.bind(evElem.type, evElem.handler);
+                elem.bind(evElem.type, evElem.handler);
             });
             this.eventList = [];
             return this;
